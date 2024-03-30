75838741a11093a4cf0998c74fcab35a6a75be78
8-Lambda-8 <j.wasle111@gmail.com>
2024-03-31 00:42:58 +0100

    added movement functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,21 @@
 #define upPin 14
 #define downPin 15
 
+void UP() {
+  digitalWrite(upPin, LOW);
+  digitalWrite(downPin, HIGH);
+};
+
+void DOWN() {
+  digitalWrite(upPin, LOW);
+  digitalWrite(downPin, HIGH);
+};
+
+void STOP() {
+  digitalWrite(upPin, LOW);
+  digitalWrite(downPin, HIGH);
+};
+
 void setup() {
   Serial.begin(115200);
   Serial.println("SETUP");
