7a64bade9fa99a466917c7bde341ac3427e5ebd2
8-Lambda-8 <j.wasle111@gmail.com>
2024-03-31 00:34:34 +0100

    added pin config

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <Arduino.h>
 
+#define upPin 14
+#define downPin 15
+
 void setup() {
   Serial.begin(115200);
   Serial.println("SETUP");
+  
+  pinMode(upPin, OUTPUT);
+  pinMode(downPin, OUTPUT);
 }
 
 void loop() {
